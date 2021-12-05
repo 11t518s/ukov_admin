cfa977ef5a743b731b73364bfbf2f0cd1888ca18
11t518s <11t518s20161759@gmail.com>
2021-12-05 14:32:42 +0900

    remove modal popup

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,39 @@
-import Footer from './Footer';
-import React, {useState} from 'react';
-import {Route, Switch} from 'react-router';
-import Home from '../route/home';
-import Program from '../route/program';
-import Recruit from '../route/recruit';
+import Footer from "./Footer";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router";
+import Home from "../route/home";
+import Program from "../route/program";
+import Recruit from "../route/recruit";
 import Navbar from "./Navbar";
-import '../css/main.css';
+import CommonModal from "./CommonModal";
+import ModalImg from "../assets/img/modalImg.jpeg";
+import "../css/main.css";
 
 function App() {
-    const [modal, setModal] = useState(true)
+  const [modal, setModal] = useState(true);
 
-    return (
-        <>
-            <Navbar/>
-            {/*{modal ? <CommonModal setModal={setModal}*/}
-            {/*                      imgSrc={ModalImg}/>*/}
-            {/*    : <Navbar/>*/}
-            {/*}*/}
-            <Switch>
-                <Route exact path={['/', '/home']}>
-                    <Home/>
-                </Route>
-                <Route exact path='/program'>
-                    <Program/>
-                </Route>
-                <Route exact path='/recruit'>
-                    <Recruit/>
-                </Route>
-            </Switch>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      {/* {modal ? (
+        <CommonModal setModal={setModal} imgSrc={ModalImg} />
+      ) : (
+        <Navbar />
+      )} */}
+      <Switch>
+        <Route exact path={["/", "/home"]}>
+          <Home />
+        </Route>
+        <Route exact path="/program">
+          <Program />
+        </Route>
+        <Route exact path="/recruit">
+          <Recruit />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
